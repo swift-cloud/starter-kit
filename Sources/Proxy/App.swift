78ab6c496baf5527e9f72c1eb4d5f64746a4fcb9
Andrew Barba <barba@hey.com>
2022-04-02 15:52:23 -0400

    Try returning subset of headers

--- Sources/Proxy/App.swift
+++ Sources/Proxy/App.swift
@@ -4,15 +4,19 @@ import Foundation
 @main
 struct App {
     static func main() async throws {
-        print("App.main()", Date().description)
+        console.log("App.main()", Date().description)
         try await onIncomingRequest(handleIncomingRequest)
     }
 
     static func handleIncomingRequest(req: IncomingRequest, res: OutgoingResponse) async throws {
-        res.upgradeToHTTP3()
         let data = try await fetch(req, origin: "https://httpbin.org", .options(
             cachePolicy: .ttl(60, staleWhileRevalidate: 60)
         ))
-        try await res.proxy(data)
+        try await res
+            .upgradeToHTTP3()
+            .header(.contentType, data.headers[.contentType])
+            .header(.contentLength, data.headers[.contentLength])
+            .append(data.body)
+            .end()
     }
 }
