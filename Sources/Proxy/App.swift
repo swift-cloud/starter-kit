599524b66e89e662443ac3b01edad1154ad5ae46
Andrew Barba <barba@hey.com>
2022-04-04 09:19:49 -0400

    Update print

--- Sources/Proxy/App.swift
+++ Sources/Proxy/App.swift
@@ -4,11 +4,11 @@ import Foundation
 @main
 struct App {
     static func main() async throws {
-        console.log("App.main()", Date().timeIntervalSince1970)
         try await onIncomingRequest(handleIncomingRequest)
     }
 
     static func handleIncomingRequest(req: IncomingRequest, res: OutgoingResponse) async throws {
+        print(req.method.rawValue, req.url.path, req.searchParams)
         let data = try await fetch(req, origin: "https://httpbin.org", .options(
             cachePolicy: .ttl(60, staleWhileRevalidate: 60)
         ))
