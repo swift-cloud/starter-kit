64e78cb56b8ac81a5c39477b6607e245778bc007
Andrew Barba <barba@hey.com>
2022-04-06 10:18:27 -0400

    Enable http3

--- Sources/Proxy/App.swift
+++ Sources/Proxy/App.swift
@@ -10,8 +10,8 @@ struct App {
     static func handleIncomingRequest(req: IncomingRequest, res: OutgoingResponse) async throws {
         print(req.method.rawValue, req.url.path, req.searchParams)
         let data = try await fetch(req, origin: "https://httpbin.org", .options(
-            cachePolicy: .ttl(10)
+            cachePolicy: .ttl(10, staleWhileRevalidate: 30)
         ))
-        try await res.proxy(data)
+        try await res.upgradeToHTTP3().proxy(data)
     }
 }
