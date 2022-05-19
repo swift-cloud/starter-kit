ca268fe1803699c32969d92b2bd4a26de0b017d6
Andrew Barba <barba@hey.com>
2022-05-18 21:55:29 -0400

    Add swift.cloud link

--- Sources/Tokamak/App.swift
+++ Sources/Tokamak/App.swift
@@ -18,11 +18,20 @@ struct App {
         VStack(alignment: .leading) {
             Text("Hello, Tokamak")
                 .font(.title)
-            Text("This is fully dynamic server side swift app powered by Tokamak")
-            Text("The current time is: \(DateFormatter().string(from: Date()))")
+                .padding(.bottom, 10)
+            Text("This is a fully dynamic server side swift app powered by Tokamak")
+            Text("The current time is \(DateFormatter().string(from: Date()))")
             Text("Your IP address is \(req.clientIpAddress().stringValue)")
             Text("This page was dynamically rendered by edge node \(Environment.Compute.region)")
             Text("The trace id for this page was \(Environment.Compute.traceId)")
+            Spacer().frame(height: 20)
+            HStack(spacing: 5) {
+                Text("Try it yourself on")
+                Link(destination: .init(string: "https://swift.cloud")!) {
+                    Text("swift.cloud").underline().foregroundColor(.blue)
+                }
+            }
+            .font(.caption)
         }
         .padding(22)
         .background(Color.gray.opacity(0.1))
