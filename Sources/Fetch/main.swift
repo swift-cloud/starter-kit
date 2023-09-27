c9bcf482bb1b72e1f86c76251741690694e6f26d
Andrew Barba <barba@hey.com>
2023-09-27 16:56:32 -0400

    Stream

--- Sources/Fetch/main.swift
+++ Sources/Fetch/main.swift
@@ -3,8 +3,7 @@ import Compute
 try await onIncomingRequest { req, res in
     let data = try await Cache.getOrSet("xxx") {
         let res = try await fetch("https://httpbin.org/json")
-        let data = try await res.jsonObject()
-        return (data, .ttl(60))
+        return (res, .ttl(60))
     }
     console.log("state: \(data.state.rawValue) hits: \(data.hits) age: \(data.age) content-length: \(data.contentLength)")
     try await res
